a9344d6b2138b2104c72848fb569da9d1844989f
Alief Arifin M <aliefarifin99@gmail.com>
2025-04-16 06:59:22 +0700

    feat: add validator schema using zod

---
+++ src/middlewares/Validate.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodSchema } from "zod";
+import { apiResponse } from "../utils/ApiResponseUtil";
+
+export const validate = (schema: ZodSchema) => (
+    req: Request,
+    res: Response,
+    next: NextFunction
+):  void => {
+    try {
+        req.body = schema.parse(req.body)
+        next()
+    } catch (error: any) {
+        apiResponse.badRequest(res, error.errors[0].message)
+    }
+}
