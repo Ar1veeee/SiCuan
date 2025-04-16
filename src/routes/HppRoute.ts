8202c4404b9a6559591086b6e7b0625d033bef5c
Alief Arifin M <aliefarifin99@gmail.com>
2025-04-16 07:33:35 +0700

    feat(hpp): add validator schema using zod

--- src/routes/HppRoute.ts
+++ src/routes/HppRoute.ts
@@ -3,10 +3,12 @@ const router = Router()
 import { createResep, Recipes } from "../controllers/HppController";
 import verifyToken from "../middlewares/AuthMiddleware";
 import verifyUserAccess from "../middlewares/VerifyUserAccess";
+import { validate } from "../middlewares/Validate";
+import { bahanSchema } from "../validators/HppValidator";
 
 router.use(verifyToken)
 
 router.get("/:user_id/:menu_id", verifyUserAccess, Recipes)
-router.post("/:user_id/:menu_id", verifyUserAccess, createResep)
+router.post("/:user_id/:menu_id", validate(bahanSchema), verifyUserAccess, createResep)
 
 export default router;
