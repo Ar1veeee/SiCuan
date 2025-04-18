66a61efcc61d1711be7fc86f16c19ff140fe6f66
Alief Arifin M <aliefarifin99@gmail.com>
2025-04-19 06:54:11 +0700

    refactor(menu): make it clean code

--- src/services/MenuService.ts
+++ src/services/MenuService.ts
@@ -1,36 +1,36 @@
-import Menu from "../models/MenuModel";
+import MenuModel from "../models/MenuModel";
 import { ApiError } from "../exceptions/apiError";
 import { validateUserExists } from "../validators/UserValidator";
 import { validateMenuOwnership } from "../validators/MenuValidator";
 
-export const addUserMenu = async (userId: number, nama_menu: string): Promise<any> => {
+export const createMenuForUser = async (userId: number, nama_menu: string): Promise<any> => {
     await validateUserExists(userId);
 
-    const existingMenu = await Menu.existingMenu(userId, nama_menu);
+    const existingMenu = await MenuModel.existingMenu(userId, nama_menu);
     if (existingMenu) {
         throw new ApiError("Menu sudah dipakai", 400)
     }
 
-    await Menu.addMenu(userId, nama_menu);
+    await MenuModel.addMenu(userId, nama_menu);
     return "Menu Berhasil Ditambahkan";
 }
 
-export const updateUserMenu = async (userId: number, menuId: number, nama_menu: string): Promise<any> => {
+export const updateMenuForUser = async (userId: number, menuId: number, nama_menu: string): Promise<any> => {
     await validateUserExists(userId);
     await validateMenuOwnership(userId, menuId);
 
-    const updated = await Menu.updateUserMenu(userId, menuId, nama_menu);
+    const updated = await MenuModel.updateUserMenu(userId, menuId, nama_menu);
     if (!updated) {
         throw new ApiError("Menu Gagal Diperbarui", 400)
     }
     return "Menu Berhasil Diperbarui"
 }
 
-export const deleteUserMenu = async (userId: number, menuId: number): Promise<any> => {
+export const deleteMenuForUser = async (userId: number, menuId: number): Promise<any> => {
     await validateUserExists(userId);
     await validateMenuOwnership(userId, menuId);
 
-    const deleted = await Menu.deleteUserMenu(userId, menuId)
+    const deleted = await MenuModel.deleteUserMenu(userId, menuId)
     if (!deleted) {
         throw new ApiError("Menu Gagal Dihapus", 400)
     }
