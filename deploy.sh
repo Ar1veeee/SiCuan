1d13d325c561626210e63e366bfe8ef7c6d5613e
Alief Arifin M <aliefarifin99@gmail.com>
2025-05-22 07:24:36 +0700

    feat: editing environment variabel

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+NC='\033[0m' 
 
 # Configuration
-PROJECT_ID="sicuan-boyz4"
+PROJECT_ID="your-project-id"
 REGION="asia-southeast2"
 TOPIC_NAME="email-notifications"
 FUNCTION_NAME="processEmailQueue"
