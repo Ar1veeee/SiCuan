8cca760716fe2d783c6c527194116300a629dcd9
Alief Arifin M <aliefarifin99@gmail.com>
2025-07-06 00:15:34 +0700

    feat: add validate query for GET method

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -2,17 +2,21 @@ import { Request, Response, NextFunction } from 'express';
 import { z, ZodSchema } from 'zod';
 import { ApiError } from '../exceptions/ApiError';
 
-export const validate = (schema: ZodSchema) => 
+export const validate = (schema: ZodSchema) =>
     (req: Request, res: Response, next: NextFunction): void => {
-    try {
-        schema.parse(req.body);
-        next();
-    } catch (error: any) {
-        if (error instanceof z.ZodError) {
-            const errorMessages = error.issues.map((issue) => issue.message).join(', ');
-            next(ApiError.badRequest(errorMessages));
-        } else {
-            next(error);
+        try {
+            if (req.method === "GET") {
+                schema.parse(req.query);
+            } else {
+                schema.parse(req.body);
+            }
+            next();
+        } catch (error: any) {
+            if (error instanceof z.ZodError) {
+                const errorMessages = error.issues.map((issue) => issue.message).join(', ');
+                next(ApiError.badRequest(errorMessages));
+            } else {
+                next(error);
+            }
         }
-    }
-};
+    };
